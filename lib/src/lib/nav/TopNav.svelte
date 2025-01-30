48c78dab6bd088cfd89bf1d18a9aa051ef2adf11
Benoit-Welsch <56845767+Benoit-Welsch@users.noreply.github.com>
2025-01-30 13:38:27 +0000

    Fix : Topnav width

--- lib/src/lib/nav/TopNav.svelte
+++ lib/src/lib/nav/TopNav.svelte
@@ -7,9 +7,7 @@
 		display: flex;
 		gap: 15px;
 		align-items: baseline;
-		padding: 0.8rem 0.8rem 0.6rem 0.8rem;
 		padding: 10px 10px 8px 10px;
-		width: calc(100vw - 20px);
 		background-color: var(--bg-color-hover);
 	}
 
